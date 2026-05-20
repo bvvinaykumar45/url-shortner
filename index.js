2b16eb6b9ebd81ad39c98dd299bdda72a74689c8
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-20 21:45:49 +0530

    Define Authentication Middleware

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import "dotenv/config";
 import express from "express";
+import { authenticationMiddleware } from "./middlewares/auth.middleware.js";
 import userRouter from "./routes/user.routes.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.use(express.json());
+app.use(authenticationMiddleware);
 
 app.get("/", (req, res) => {
   return res.status(200).json({ status: "Server is up and running..." });
 });
 
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
